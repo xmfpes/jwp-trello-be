9167faf3613b01d2f4aea027003604773ac33a8a
KyuNam <xmfpes@naver.com>
2017-11-16 11:14:49 +0900

    Feat(Initialize) - jwp-trell-be project init

---
+++ src/main/java/com/kyunam/trello/jwptrellobe/JwpTrelloBeApplication.java
@@ -0,0 +1,12 @@
+package com.kyunam.trello.jwptrellobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwpTrelloBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwpTrelloBeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kyunam/trello/jwptrellobe/JwpTrelloBeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kyunam.trello.jwptrellobe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JwpTrelloBeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
